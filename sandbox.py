bfde4b6b16192e7201ec84084bf754e24fdc37e9
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-11-24 15:00:18 -0700

    chore: Completed strings tutorial

--- sandbox.py
+++ sandbox.py
@@ -1,26 +1,44 @@
 # Intro tutorial to Python Progammer Bootcamp | Giles McMullen-Klein | input by Ryan Buchanan 21NOV20
 
 
-print('Hello world!')
+# print('Hello world!')
 
-x = 5
+# x = 5
 
-y = 12
+# y = 12
 
-z = x + y 
+# z = x + y 
 
-print(z)
+# print(z)
 
-new_variable = 13
+# new_variable = 13
 
-z = x - y
+# z = x - y
 
-a, b, c = 2.5, 3.14159, 1
+# a, b, c = 2.5, 3.14159, 1
 
-c = b * a**2
+# c = b * a**2
 
-radius = 2.5
+# radius = 2.5
 
-pi = 3.14159
+# pi = 3.14159
 
-area = pi * radius**2
+# area = pi * radius**2
+
+# phrase_1 = 'The cat sat on the mat!'
+
+# phrase_2 = 'And so did the dog!'
+
+# phrase_3 = phrase_1 + ' ' + phrase_2
+
+# print(phrase_3)
+
+# user_input = int(input('How many apples do you have?\n>>> '))
+
+# type(user_input)
+
+# help()
+
+# while = 5
+
+
